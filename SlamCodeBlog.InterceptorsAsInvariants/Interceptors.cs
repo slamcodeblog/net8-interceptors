9fd0abe33c7bedf96cf1861f1b528ef5e44ecdad
Slawomir Moriak <slawomir.moriak@ihsmarkit.com>
2023-12-12 16:24:21 +0100

    Add project files.

---
+++ SlamCodeBlog.InterceptorsAsInvariants/Interceptors.cs
@@ -0,0 +1,16 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    sealed file class InterceptsLocationAttribute(string filePath, int line, int column) : Attribute
+    {
+    }
+
+    public static class Interceptors
+    {
+        [InterceptsLocation(
+            "C:\\Projects\\Own\\repos\\SlamCodeBlog\\SlamCodeBlog.InterceptorsAsInvariants\\SlamCodeBlog.InterceptorsAsInvariants\\Program.cs", 
+            3,
+            8)]
+        public static void DoRunIntercepted() => Console.WriteLine("Do run intercepted!");
+    }
+}
